57874619243f84805334e968df0671469a2e8ae0
Tim Hochberg <bitsofbits@users.noreply.github.com>
2025-01-06 11:02:47 -0700

    Provide instructions for how to download data from download portal and then code to process it into rasters

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 ![Vessel presence shown on a world map in the Spilhaus projection](presence_map.png "Vessel Presence")
 
-Plot vessel presence with the Spilhaus projection, with possibly eventual aim
-of making work-alike class usable with Cartopy.
+Plot vessel presence with the Spilhaus projection.
 
 N.B. `spilhaus.py` and contains GPL-3 licensed code from https://github.com/rtlemos/spilhaus
 so this entire repo is GPL-3 licensed rather than our more typical Apache license.
--- notebooks/PlotPresence.py
+++ notebooks/PlotPresence.py
@@ -19,14 +19,55 @@
 # more convential projections, but it suffers from some pretty serious size distortions. Most dramatically, the Chinese
 # coast appears much larger in this projection than it should.
 
+import csv
 import numpy as np
+import pandas as pd
 import sys
+import tifffile
+import zipfile
+from datetime import datetime, timedelta
 sys.path.append('..')
 from spilhaus.plot_spilhaus import plot_spilhaus
 
-presence_raster = np.load('./presence_example.npz')['presence']
-ocean_raster = np.load('./ocean.npz')['ocean']
+# ### Grab some example data
+#
+# Download `bathymetry.zip` from https://globalfishingwatch.org/data-download/datasets/public-bathymetry-v1. You'll
+# need to register for a free account. I found that Safari had problems downloading from the our public data, so you
+# may want to use a different browser. Copy the file to the `untracked` directory or edit the location below.
+#
+# Then download `fleet-daily-csvs-100-v2-2020.zip` from https://globalfishingwatch.org/data-download/datasets/public-fishing-effort.
 
-plot_spilhaus(presence_raster, ocean_raster, resolution=6000, dpi=72)
+bathymetry_path = '../untracked/bathymetry.zip'
+with zipfile.ZipFile(bathymetry_path) as archive:
+    with archive.open('bathymetry.tif') as f:
+        bathymetry_raster = tifffile.imread(f)
+# Bathymetry raster is upside down from how plot_spilhaus expects it, so flip it
+is_ocean = bathymetry_raster[::-1] < 0
 
+# +
+fleet_path = '../untracked/fleet-daily-csvs-100-v2-2020.zip'
+start_date = datetime(2020, 1, 1)
+end_date = datetime(2020, 12, 31) 
 
+# Fishing data is stored in a stack of daily CSVs, so add them up
+# This takes a few minutes.
+activity_raster = np.zeros([180 * 100, 360 * 100])
+date = start_date
+with zipfile.ZipFile(fleet_path) as archive:
+    while date <= end_date:
+        # Provide some feedback so we can tell something is happening
+        if date.day == 1:
+            if date != start_date:
+                print()
+            print(f'{date.month:>2d}:', end='')
+        print('.', flush=True, end='')
+        with archive.open(f'fleet-daily-csvs-100-v2-2020/{date:%Y-%m-%d.csv}') as f:
+            df = pd.read_csv(f)
+            i = ((df.cell_ll_lat.values + 90) * 100).astype(int)
+            j = ((df.cell_ll_lon.values + 180) * 100).astype(int)
+            activity_raster[i, j] += df.hours
+        date += timedelta(days=1)
+# -
+
+# This will also take a few minutes
+plot_spilhaus(activity_raster, is_ocean, resolution=3000, dpi=72, vmin=0.001, vmax=10)
